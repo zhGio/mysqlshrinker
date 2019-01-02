603909b20f3aa27c15fa5c6f2796ad21560a00fd
Ognjen Mišić <ognjen.misic@codecentric.de>
2019-01-02 22:47:00 +0100

    update main() with some key constants

--- src/main/java/zhgio/myss/MySqlShrinkerApplication.java
+++ src/main/java/zhgio/myss/MySqlShrinkerApplication.java
@@ -4,16 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
-@Log4j2
+@Slf4j
 public class MySqlShrinkerApplication {
 
-	static final String USER_NAME = "?";
-	static final String PASSWORD = "?";
-	static final String DB_ENGINE = "?";
-	static final String DEFAULT_CHARSET = "?";
+	public static final String DESTINATION_SCHEMA_URL = "jdbc:mysql://localhost:3306/";
+	public static final String DESTINATION_SCHEMA_NAME = "ina_trimmed";
+	public static final String DESTINATION_USERNAME = "root";
+	public static final String DESTINATION_PASSWORD = "root";
+
+	public static final String ORIGIN_SCHEMA_URL = "jdbc:mysql://localhost:3355/";
+	public static final String ORIGIN_SCHEMA_NAME = "?";
+	public static final String ORIGIN_USERNAME = "?";
+	public static final String ORIGIN_PASSWORD = "?";
+
+	public static final String DB_ENGINE = "InnoDB";
+	public static final String DEFAULT_CHARSET = "latin1";
+	public static final String DB_DRIVER = "com.mysql.jdbc.Driver";
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(MySqlShrinkerApplication.class, args);
